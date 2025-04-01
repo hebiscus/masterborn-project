c2054507e128724f984eb6b2f3ecff5cbe2b73f4
hebiscus <akikopoland@gmail.com>
2025-04-01 11:54:39 +0200

    feat: add create candidate endpoint

--- new-recruitment-api/src/candidates.controller.ts
+++ new-recruitment-api/src/candidates.controller.ts
@@ -22,7 +22,53 @@ export class CandidatesController {
         }
     }
 
-    create(req: Request, res: Response) {
-        res.json({});
+    async create(req: Request, res: Response) {
+        const db = await setupDb();
+
+        const {
+            first_name,
+            last_name,
+            email,
+            phone_number,
+            years_of_experience,
+            recruiter_notes,
+            recruitment_status,
+            consent_date,
+            job_offer_id
+        } = req.body
+
+        if (!first_name || !last_name || !email || !phone_number || !years_of_experience || !consent_date) {
+            return res.status(400).json({ error: "Some body data missing" })
+        }
+
+        try {
+            const jobOffer = await db.get('SELECT * FROM JobOffer WHERE id = ?', [job_offer_id])
+            if (!jobOffer) {
+                return res.status(404).json({ error: "You have to supply a job offer to create a candidate" })
+            }
+
+            await db.run(
+                `INSERT INTO Candidate (first_name, last_name, email, phone_number, years_of_experience, recruiter_notes, recruitment_status, consent_date, job_offer_id)
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+                [
+                    first_name,
+                    last_name,
+                    email,
+                    phone_number,
+                    years_of_experience,
+                    recruiter_notes || null,
+                    recruitment_status || 'new',
+                    consent_date,
+                    job_offer_id
+                ]
+            );
+
+
+            return res.status(201).json("Successfully created a new candidate!")
+        } catch (error) {
+            console.error("Error wgile creating candidate:", error)
+            return res.status(500).json({ error: "Internal server error" })
+        }
+
     }
 }
